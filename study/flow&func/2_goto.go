2506fce71700e840ff016be62abf2dba03e644c7
andyadc <andaicheng@gmail.com>
2017-12-06 18:24:41 +0800

    Hello Go Flow goto

---
+++ study/flow&func/2_goto.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+用goto跳转到必须在当前函数内定义的标签
+
+	func main() {
+		i := 0
+	Here: // 这行的第一个词，以冒号结束作为标签
+		fmt.Println(i)
+		i ++
+		goto Here // 跳转到Here去
+	}
+
+***标签名是大小写敏感的。
+
+*/
+
+func main() {
+
+}
