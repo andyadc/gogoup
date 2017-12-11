72bcd5b5835adf56acec1e4823a56a3c1ae2301a
andyadc <andaicheng@gmail.com>
2017-12-11 18:37:57 +0800

    Go Database Cookie

--- web/basic/Hello.go
+++ web/basic/Hello.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -44,6 +45,12 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Form {
 		log.Println("key: ", k, ", val: ", strings.Join(v, ``))
 	}
+
+	// 设置cookie
+	expiration := time.Now()
+	expiration = expiration.AddDate(0, 0, 1)
+	cookie := http.Cookie{Name: `go-cookie-hello`, Value: `Hello Go`, Expires: expiration, HttpOnly: true}
+	http.SetCookie(w, &cookie)
 
 	fmt.Fprint(w, "Hello Go") //这个写入到w的是输出到客户端的
 }
