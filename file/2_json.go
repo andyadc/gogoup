fbc40b08344000cd29749a64b2538e658252463c
andyadc <andaicheng@gmail.com>
2017-12-13 11:20:34 +0800

    Go JSON

--- file/2_json.go
+++ file/2_json.go
@@ -9,21 +9,57 @@ import (
 
 {"servers":[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},{"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}
 
+struct tag
+`json:"servers`
+
+Go类型和JSON类型的对应关系如下：
+	bool 代表 JSON booleans,
+	float64 代表 JSON numbers,
+	string 代表 JSON strings,
+	nil 代表 JSON null.
 
 */
 
 type ServerSlice struct {
-	Servers []Server
+	Servers []Server `json:"servers"`
 }
 
 type Server struct {
-	ServerName string
-	ServerIP   string
+	ID         int    `json:"-"`
+	Address    string `json:"adress,omitempty"`
+	ServerName string `json:"serverName"`
+	ServerIP   string `json:"serverIP"`
 }
 
 func main() {
 	//readJson()
 	//parseJson()
+	writeJson()
+}
+
+/**
+针对JSON的输出，我们在定义struct tag的时候需要注意的几点是:
+	字段的tag是"-"，那么这个字段不会输出到JSON
+	tag中带有自定义名称，那么这个自定义名称会出现在JSON的字段名中，例如上面例子中serverName
+	tag中如果带有"omitempty"选项，那么如果该字段值为空，就不会输出到JSON串中
+	如果字段类型是bool, string, int, int64等，而tag中带有",string"选项，那么这个字段在输出到JSON的时候会把该字段对应的值转换成JSON字符串
+
+Marshal函数只有在转换成功的时候才会返回数据，在转换的过程中我们需要注意几点：
+	JSON对象只支持string作为key，所以要编码一个map，那么必须是map[string]T这种类型(T是Go语言中任意的类型)
+	Channel, complex和function是不能被编码成JSON的
+	嵌套的数据是不能编码的，不然会让JSON编码进入死循环
+	指针在编码的时候会输出指针指向的内容，而空指针会输出null
+*/
+func writeJson() {
+	var ss ServerSlice
+	ss.Servers = append(ss.Servers, Server{ID: 1, ServerName: `a`, ServerIP: `123`})
+	ss.Servers = append(ss.Servers, Server{ID: 2, ServerName: `b`, ServerIP: `321`, Address: `shanghai`})
+
+	b, err := json.Marshal(ss)
+	if err != nil {
+		fmt.Println("json err:", err)
+	}
+	fmt.Println(string(b))
 }
 
 func parseJson() {
